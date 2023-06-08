b9441a8330e0ea7dad4eb82a963b0090a7adb389
Shaharyar07 <shaharyar.tariq248@gmail.com>
2023-06-09 02:55:52 +0500

    minor fixes

--- src/screens/Categories/CategoriesScreen.js
+++ src/screens/Categories/CategoriesScreen.js
@@ -1,13 +1,47 @@
-import React, { useLayoutEffect } from "react";
-import { FlatList, Text, View, Image, TouchableHighlight } from "react-native";
+import React, { useLayoutEffect, useEffect, useState } from "react";
+import {
+  FlatList,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  ActivityIndicator,
+} from "react-native";
 import styles from "./styles";
 import { categories } from "../../data/dataArrays";
 import { getNumberOfRecipes } from "../../data/MockDataAPI";
 import MenuImage from "../../components/MenuImage/MenuImage";
-
+import {
+  doc,
+  collection,
+  setDoc,
+  getDoc,
+  deleteField,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 export default function CategoriesScreen(props) {
-  const { navigation } = props;
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
+  const { navigation } = props;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const docRef = collection(db, "categories");
+        const docSnap = await getDocs(docRef);
+        docSnap.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+        });
+
+        setData(docSnap.docs.map((doc) => doc.data()));
+        setLoading(false);
+      } catch (err) {
+        console.log("error in catch: ", err);
+      }
+    }
+    fetchData();
+  }, []);
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitleStyle: {
@@ -34,9 +68,15 @@ export default function CategoriesScreen(props) {
   };
 
   const renderCategory = ({ item }) => (
-    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressCategory(item)}>
+    <TouchableHighlight
+      underlayColor='rgba(73,182,77,0.9)'
+      onPress={() => onPressCategory(item)}
+    >
       <View style={styles.categoriesItemContainer}>
-        <Image style={styles.categoriesPhoto} source={{ uri: item.photo_url }} />
+        <Image
+          style={styles.categoriesPhoto}
+          source={{ uri: item.photo_url }}
+        />
         <Text style={styles.categoriesName}>{item.name}</Text>
         <Text style={styles.categoriesInfo}>{getNumberOfRecipes(item.id)} recipes</Text>
       </View>
@@ -45,7 +85,15 @@ export default function CategoriesScreen(props) {
 
   return (
     <View>
-      <FlatList data={categories} renderItem={renderCategory} keyExtractor={(item) => `${item.id}`} />
+      {loading ? (
+        <ActivityIndicator size='large' color='#0000ff' />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderCategory}
+          keyExtractor={(item) => `${item.id}`}
+        />
+      )}
     </View>
   );
 }
